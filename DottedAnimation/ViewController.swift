74bb64bfa35cd00c1e0632409d424964d62c5504
Varun Sharma <varun.sharma@techolution.com>
2023-08-28 15:50:56 +0530

    Initial Commit

---
+++ DottedAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DottedAnimation
+//
+//  Created by Varun Sharma on 28/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
